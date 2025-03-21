5822b580ae385c88c9897999276d1ee925a6d900
Jay Diyyala <101083288+jayconsensys@users.noreply.github.com>
2025-03-21 10:56:35 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,9 +4,15 @@ set -e
 echo "Generating Prisma client..."
 yarn run prisma:generate || { echo "Prisma generate failed"; exit 1; }
 
+echo -e "\n===========================\n"
+echo "Run Prisma DB migrations..."
+yarn prisma migrate deploy || { echo "prisam DB migration failed"; exit 1; }
+
+echo -e "\n===========================\n"
 echo "Generating Prisma SQL..."
 yarn run prisma:generate-sql || { echo "Prisma SQL generation failed"; exit 1; }
 
+echo -e "\n===========================\n"
 echo "Building application..."
 yarn build || { echo "Build failed"; exit 1; }
 
@@ -16,5 +22,6 @@ if [ ! -f dist/main.js ]; then
   exit 1
 fi
 
+echo -e "\n===========================\n"
 echo "Starting application..."
 exec node dist/main.js
